e97c88bcd17cd78d46ead4b2afbd51302de52732
SaltySalt77 <alexiena@naver.com>
2023-07-08 13:39:56 +0900

    feat: Set state variable update for selected category

--- front-end/src/components/category/categoryItem.tsx
+++ front-end/src/components/category/categoryItem.tsx
@@ -6,10 +6,25 @@ const CategoryItem: React.FC<{
   selected: string;
   setSelected: (selected: string) => void;
 }> = (props) => {
+  let className = Style.categoryName;
+
+  if (props.selected == props.categoryName) {
+    className = Style.selected;
+  }
+
+  const onClickHandler = (event: React.MouseEvent) => {
+    if (props.selected == props.categoryName) {
+      props.setSelected("" as string);
+    } else {
+      props.setSelected(props.categoryName);
+    }
+    console.log(props.selected);
+  };
+
   return (
     <div className={Style.category}>
-      <Button>
-        <h3>{props.categoryName}</h3>
+      <Button onClick={onClickHandler}>
+        <h3 className={className}>{props.categoryName}</h3>
       </Button>
       <hr color="white" />
     </div>
