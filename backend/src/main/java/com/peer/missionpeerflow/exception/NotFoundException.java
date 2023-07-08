641cae5c68071b9aa0f8741d0574aebb35670d37
perman0519 <songpil10117@naver.com>
2023-07-08 20:02:18 +0900

    delete: backend dir

--- backend/src/main/java/com/peer/missionpeerflow/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.peer.missionpeerflow.exception;
-
-public class NotFoundException extends RuntimeException {
-
-	public NotFoundException(String message) {
-		super(message);
-	}
-}
--- backend/src/main/java/com/peer/missionpeerflow/repository/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.peer.missionpeerflow.repository;
-
-import com.peer.missionpeerflow.entity.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-}
