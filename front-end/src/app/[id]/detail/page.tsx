00cf5c4332769b089093e351e3e5d370485d9a5b
SaltySalt77 <alexiena@naver.com>
2023-07-14 21:50:11 +0900

    feat: Set up dynamic routing successfully

---
+++ front-end/src/app/[id]/detail/page.tsx
@@ -0,0 +1,9 @@
+const Page: React.FC<{ params: { id: string } }> = (props) => {
+  return (
+    <div
+      style={{ height: "100vh", width: "100vw", backgroundColor: "#000" }}
+    ></div>
+  );
+};
+
+export default Page;
