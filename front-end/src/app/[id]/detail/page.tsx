3d50595575b2bca37a7bb8fbfc6ca91c5106a616
SaltySalt77 <alexiena@naver.com>
2023-07-16 21:00:24 +0900

    feat: Connect API to fetch detailed question information

--- front-end/src/app/[id]/detail/page.tsx
+++ front-end/src/app/[id]/detail/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Styles from "./page.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import QuestionDetail from "@/components/detail/questionDetail/questionDetail";
 import AnswerDetail from "@/components/detail/questionAnswerDetail/answerDetail";
 import AnswerForm from "@/components/detail/answerForm/answerForm";
 import { styled } from "styled-components";
 import Link from "next/link";
+import axios from "axios";
+import LoadingBackdrop from "@/components/common/loadingBackdrop";
 
 const Button = styled.button`
   display: block;
@@ -17,106 +19,91 @@ const Button = styled.button`
   border-width: 1.5px;
 `;
 
+type Answer = {
+  nickname: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string | null;
+  recommend: number;
+  questionId: number;
+  answerId: number;
+  adopted: boolean;
+};
+
+type Question = {
+  type: string;
+  category: string;
+  nickname: string;
+  title: string;
+  content: string;
+  recommend: number;
+  view: number;
+  createdAt: string;
+  updatedAt: string | null;
+  answerList: Answer[];
+};
+
 const Page: React.FC<{ params: { id: string } }> = (props) => {
   const [hasError, setHasError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [content, setContent] = useState({} as Question);
+  const [errorMessage, setErrorMessage] = useState("" as string);
 
-  const content = {
-    type: "question",
-    category: "FDF",
-    nickname: "san",
-    title: "dummy",
-    content: "다시 질문 드립니다 fdf는 어떤 과제인가요?",
-    recommend: 0,
-    view: 1,
-    createdAt: "2023-07-16T16:32:13.136259",
-    updatedAt: null,
-    answerList: [
-      {
-        nickname: "jun",
-        content: "이건 1번 질문의 2번 답변 입니다. success.",
-        createdAt: "2023-07-16T18:29:19.843061",
-        updatedAt: null,
-        recommend: 0,
-        questionId: 10,
-        answerId: 1,
-        adopted: false,
-      },
-      {
-        nickname: "jun",
-        content: "이건 1번 질문의 2번 답변 입니다. success.",
-        createdAt: "2023-07-16T18:29:20.812388",
-        updatedAt: null,
-        recommend: 0,
-        questionId: 10,
-        answerId: 2,
-        adopted: false,
-      },
-      {
-        nickname: "jun",
-        content: "이건 1번 질문의 2번 답변 입니다. success.",
-        createdAt: "2023-07-16T18:29:21.616024",
-        updatedAt: null,
-        recommend: 0,
-        questionId: 10,
-        answerId: 3,
-        adopted: false,
-      },
-      {
-        nickname: "jun",
-        content: "이건 1번 질문의 2번 답변 입니다. success.",
-        createdAt: "2023-07-16T18:29:22.334449",
-        updatedAt: null,
-        recommend: 0,
-        questionId: 10,
-        answerId: 4,
-        adopted: false,
-      },
-      {
-        nickname: "jun",
-        content: "이건 1번 질문의 2번 답변 입니다. success.",
-        createdAt: "2023-07-16T18:29:23.085986",
-        updatedAt: null,
-        recommend: 0,
-        questionId: 10,
-        answerId: 5,
-        adopted: false,
-      },
-    ],
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      axios
+        .get(`http://localhost:8000/v1/question/${props.params.id}`)
+        .then((res) => {
+          setContent(res.data);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          if (error?.response?.data?.error?.message) {
+            setErrorMessage(error?.response?.data?.error?.message);
+          }
+          setHasError(true);
+          setIsLoading(false);
+        });
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className={Styles.main}>
       <div></div>
       <div className={Styles.mainThread}>
-        <QuestionDetail
-          category={content.category}
-          nickname={content.nickname}
-          title={content.title}
-          content={content.content}
-          recommend={content.recommend}
-          view={content.view}
-          createdAt={content.createdAt}
-          updatedAt={content.updatedAt}
-          isLoading={isLoading}
-          hasError={hasError}
-          id={parseInt(props.params.id)}
-        />
-        {content.answerList.map((aContent) => {
-          return (
-            <AnswerDetail
-              key={aContent.answerId}
-              nickname={aContent.nickname}
-              content={aContent.content}
-              recommend={aContent.recommend}
-              createdAt={aContent.createdAt}
-              updatedAt={aContent.updatedAt}
-              isLoading={isLoading}
-              hasError={hasError}
-              id={aContent.answerId}
-            />
-          );
-        })}
+        {content?.category && (
+          <QuestionDetail
+            category={content.category}
+            nickname={content.nickname}
+            title={content.title}
+            content={content.content}
+            recommend={content.recommend}
+            view={content.view}
+            createdAt={content.createdAt}
+            updatedAt={content.updatedAt}
+            isLoading={isLoading}
+            hasError={hasError}
+            id={parseInt(props.params.id)}
+          />
+        )}
+        {!isLoading &&
+          !hasError &&
+          content?.answerList?.map((aContent) => {
+            return (
+              <AnswerDetail
+                key={aContent.answerId}
+                nickname={aContent.nickname}
+                content={aContent.content}
+                recommend={aContent.recommend}
+                createdAt={aContent.createdAt}
+                updatedAt={aContent.updatedAt}
+                isLoading={isLoading}
+                hasError={hasError}
+                id={aContent.answerId}
+              />
+            );
+          })}
         <AnswerForm questionId={parseInt(props.params.id)} />
       </div>
       <div className={Styles.sideMenu}>
@@ -136,6 +123,12 @@ const Page: React.FC<{ params: { id: string } }> = (props) => {
           </Link>
         </Button>
       </div>
+      <LoadingBackdrop
+        hasError={hasError}
+        setHasError={setHasError}
+        errorMessage={errorMessage}
+        isLoading={isLoading}
+      />
     </div>
   );
 };
