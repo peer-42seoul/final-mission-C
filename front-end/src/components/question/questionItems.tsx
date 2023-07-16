a339f1f18593de2c4d0d01b2227b7aa65cdf0e65
SaltySalt77 <alexiena@naver.com>
2023-07-16 16:15:45 +0900

    feat: Add useDeleteAPI hook

--- front-end/src/components/question/questionItems.tsx
+++ front-end/src/components/question/questionItems.tsx
@@ -2,22 +2,29 @@
 import type questionItemElement from "../../types/questionItemElement";
 import QuestionItem from "./questionItem";
 import Styles from "./question.module.css";
+import { useRouter } from "next/navigation";
 
 const QuestionItems: React.FC<{
   items: questionItemElement[];
   sort: string;
   setSelected: (selected: string) => void;
-}> = (props) => (
-  <div>
-    {props.items?.map((item) => (
-      <QuestionItem
-        key={item.questionId}
-        item={item}
-        sort={props.sort}
-        setSelected={props.setSelected}
-      />
-    ))}
-  </div>
-);
+}> = (props) => {
+  const route = useRouter();
+
+  return (
+    <div>
+      {props.items?.map((item) => (
+        <div onClick={() => route.push(`/${item.questionId}/detail`)}>
+          <QuestionItem
+            key={item.questionId}
+            item={item}
+            sort={props.sort}
+            setSelected={props.setSelected}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default QuestionItems;
